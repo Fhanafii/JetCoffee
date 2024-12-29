acc974b7aef621caf0a427eeaafb7efe409d521b
Fhanafii <fhanafi544@gmail.com>
2024-12-29 14:46:54 +0700

    Using slotBased layout for reslove dry code

--- app/src/main/java/com/fhanafi/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/fhanafi/jetcoffee/MainActivity.kt
@@ -24,6 +24,7 @@ import com.fhanafi.jetcoffee.model.dummyBestSellerMenu
 import com.fhanafi.jetcoffee.model.dummyCategory
 import com.fhanafi.jetcoffee.model.dummyMenu
 import com.fhanafi.jetcoffee.ui.components.CategoryItem
+import com.fhanafi.jetcoffee.ui.components.HomeSection
 import com.fhanafi.jetcoffee.ui.components.MenuItem
 import com.fhanafi.jetcoffee.ui.components.Search
 import com.fhanafi.jetcoffee.ui.components.SectionText
@@ -52,12 +53,18 @@ class MainActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Banner()
-        SectionText(stringResource(R.string.section_category))
-        CategoryRow()
-        SectionText(stringResource(R.string.section_favorite_menu))
-        MenuRow(dummyMenu)
-        SectionText(stringResource(R.string.section_best_seller_menu))
-        MenuRow(dummyBestSellerMenu)
+        HomeSection(
+            title = stringResource(R.string.section_category),
+            content = { CategoryRow() }
+        )
+        HomeSection(
+            title = stringResource(R.string.menu_favorite),
+            content = { MenuRow(dummyMenu)}
+        )
+        HomeSection(
+            title = stringResource(R.string.section_best_seller_menu),
+            content = { MenuRow(dummyBestSellerMenu)}
+        )
     }
 }
 
